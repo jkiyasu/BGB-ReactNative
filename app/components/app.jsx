a7d055db107b9cd333bc087e718d22b2b15ad08b
Sarah Soares <sjsoares@berkeley.edu>
2021-03-08 15:22:46 -0800

    working on openlayers

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -9,38 +9,51 @@ import DayComponent from "./day/day_component";
 import ScheduleComponent from "./schedule/schedule_component";
 import ProfileComponent from "./profile/profile_component";
 
+// map stuff
+import Map from "./Map";
+import { Layers, TileLayer, VectorLayer } from "./Layers";
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import { osm, vector } from "./Source";
+import { fromLonLat, get } from 'ol/proj';
+import GeoJSON from 'ol/format/GeoJSON';
+
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [center, setCenter] = useState([-94.9065, 38.9884]);
+	const [zoom, setZoom] = useState(9);
   return(
-    <NavigationContainer>
-      <Stack.Navigator
-      screenOptions={{
-        headerShown: false
-      }}
-      >
-        <Stack.Screen 
-          name="Login" 
-          component={LoginOptionsComponent}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeComponent}
-        />
-        <Stack.Screen 
-          name="Day" 
-          component={DayComponent}
-        />
-        <Stack.Screen 
-          name="Schedule" 
-          component={ScheduleComponent}
-        />
-        <Stack.Screen 
-          name="Profile" 
-          component={ProfileComponent}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator
+    //   screenOptions={{
+    //     headerShown: false
+    //   }}
+    //   >
+    //     <Stack.Screen 
+    //       name="Login" 
+    //       component={LoginOptionsComponent}
+    //     />
+    //     <Stack.Screen
+    //       name="Home"
+    //       component={HomeComponent}
+    //     />
+    //     <Stack.Screen 
+    //       name="Day" 
+    //       component={DayComponent}
+    //     />
+    //     <Stack.Screen 
+    //       name="Schedule" 
+    //       component={ScheduleComponent}
+    //     />
+    //     <Stack.Screen 
+    //       name="Profile" 
+    //       component={ProfileComponent}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+      <div>
+        <Map center={fromLonLat(center)} zoom={zoom}></Map>
+      </div>
   )
 };
 
